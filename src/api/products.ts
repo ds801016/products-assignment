7eac232ba1cd17c76978a2ca9a71cb71090eb4cc
ds801016 <64468218+ds801016@users.noreply.github.com>
2024-06-02 20:11:46 +0530

    api response console

--- src/api/products.ts
+++ src/api/products.ts
@@ -4,6 +4,7 @@ import { ResponseType } from "@/types/general";
 
 export const getProducts = async () => {
   const response: ResponseType = await myAxios.get("/products");
+  console.log("this is the api response", response);
   if (!response?.length && typeof response !== "object") {
     throw new Error("Response is not in valid format");
   }
